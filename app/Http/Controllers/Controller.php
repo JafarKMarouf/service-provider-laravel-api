d2561fb3b5ed591308b476a34539911f5f77c362
Jafar Marouf <18597109-JafarMaruf@users.noreply.gitlab.com>
2024-04-18 17:19:45 -0500

    create fake images for services

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\URL;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+	use AuthorizesRequests, ValidatesRequests;
+
+	public function saveImage($image, $ext, $path = '/public')
+	{
+
+		$filename = time() . '.' . $ext;
+		$image->storeAs($filename, $image);
+		return URL::to('/') . '/storage/' . $path . '/' . $filename;
+	}
 }
